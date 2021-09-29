bed9ab16ca0a6d2f7644c96dbe604915d7e42f21
DaffaAlwanMuktie <12200594@bsi.ac.id>
2021-09-29 18:44:35 +0700

    tugas-template

---
+++ application/views/v_index.php
@@ -0,0 +1,12 @@
+<article>
+                <div class="isi">
+                    <div class="judul">
+                        Ikan Badut
+                    </div>
+                    <img src="<?php echo base_url() ?>assets/img/ikanBadut.jpg" height="100px" width="120px" >
+                    <p>Ikan badut ini dapat dikenali dengan warna jingganya ikan ini tumbuh mencapai 8 cm serta termasuk dalam ikan terpopuler didunia. Hewan ini masuk kedalam Genus Amphiprion. Ikan giru atau lebih dikenal dengan sebutan ikan badut adalah ikan dari anak suku Amphiprioninae dalam suku Pomacentridae. Ada 28 spesies yang biasa dikenali, salah satunya adalah genus Premnas, sementara sisanya termasuk dalam genus Amphiprion. </p>
+                    <p>Mereka tersebar di lautan Pasifik, Laut Merah, lautan India, dan karang besar Australia. Di alam bebas mereka bersimbiosis dengan anemon laut. Anemon akan melindungi Ikan badut dari pemangsa dan Ikan badut akan membersihkan Anemon dengan memakan sisa - sisa makanan Anemon. Ikan badut berwarna kuning, jingga, kemerahan atau kehitaman. </p>
+                    <p>Spesies terbesar mencapai panjang 18 cm, sementara yang terkecil hanya 6 cm. Mereka, pada umumnya, dijumpai pada laguna-laguna berbatu di seputar terumbu karang, atau pada daerah koastal dengan kedalaman kurang dari 50 meter dan berair jernih. Di perairan Papua New Guinea, bisa ditemukan ikan badut tidak kurang dari 8 spesies. Di alam, ikan badut mengkonsumsi zooplankton, udang-udangan dan algae yang dijumpai di habitat mereka.</p>
+                </div>
+            
+         </article>
